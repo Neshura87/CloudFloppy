a80e97a97a03f4de96c1958d363e3f5e326cde11
Ryhon <ryhon0@gmail.com>
2022-07-23 16:51:41 +0200

    Abstract SyncProvider class

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace GameSync
+﻿using GameSync;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+DateTime GetLatestModifiedTime(string dir, Regex includeRegex, Regex excludeRegex)
 {
-    class Program
-    {
-        static public void Main(string[] args)
-        {
-            Nextcloud api = new Nextcloud();
-        }
-    }
+	return Directory.GetFiles(dir, "*", SearchOption.AllDirectories)
+		.Where(f => !excludeRegex.IsMatch(f))
+		.Where(f => includeRegex.IsMatch(f))
+		.Max(f => File.GetLastAccessTime(f));
+}
+
+SyncProvider provider = null;
+string gameId = "my-game";
+string saveDir = "saves";
+Regex includeRegex = new Regex("*");
+Regex excludeRegex = new Regex("screenshots", RegexOptions.IgnoreCase);
+
+// Sync saves to local
+{
+	DateTime? lastSyncTime = await provider.GetLastSyncTime(gameId);
+	if (lastSyncTime == null)
+		Console.WriteLine("Game " + gameId + " not on remote server");
+
+	DateTime localModifiedTime = GetLatestModifiedTime(saveDir, includeRegex, excludeRegex);
+
+	if (lastSyncTime > localModifiedTime)
+	{
+		Console.WriteLine("Local save out of date, syncing...");
+		await provider.DownloadFiles(gameId, saveDir);
+		Console.WriteLine("Saves synced!");
+	}
+}
+
+// Launch game and wait for exit
+
+// Sync saves to remote
+{
+	DateTime? lastSyncTime = await provider.GetLastSyncTime(gameId);
+	if (lastSyncTime == null)
+		Console.WriteLine("Game " + gameId + " not on remote server");
+
+	DateTime localModifiedTime = GetLatestModifiedTime(saveDir, includeRegex, excludeRegex);
+
+	if (lastSyncTime < localModifiedTime)
+	{
+		Console.WriteLine("Remote save out of date, syncing...");
+		await provider.UploadFiles(gameId, saveDir);
+		Console.WriteLine("Saves synced!");
+	}
 }
---
+++ src/SyncProviders/SyncProvider.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace GameSync;
+
+public class SpaceUssage
+{
+	/// <summary>
+	/// Total available space in bytes.
+	/// </summary>
+	public ulong TotalSpace;
+	/// <summary>
+	/// Available space in bytes.
+	/// </summary>
+	public ulong FreeSpace;
+}
+
+public abstract class SyncProvider
+{
+	/// <summary>
+	/// Get the last time saves for a game were synced.
+	/// </summary>
+	/// <param name="gameId">The ID of the game to check</param>
+	/// <returns>Last sync time or null if never synced</returns>
+	public abstract Task<DateTime?> GetLastSyncTime(string gameId);
+	
+	/// <summary>
+	/// Return a list of files stored for a game.
+	/// </summary>
+	/// <param name="gameId">The ID of the game to check</param>
+	/// <returns>IEnumerable of relative file paths</returns>
+	public abstract Task<List<string>> ListFiles(string gameId);
+
+	/// <summary>
+	/// Downloads all the remote files to a local directory.
+	/// </summary>
+	/// <param name="gameId">The ID of the game to download files for</param>
+	/// <param name="outDir">The output directory</param>
+	public abstract Task DownloadFiles(string gameId, string outDir);
+
+	/// <summary>
+	/// Upload files from local directory to remote server.
+	/// </summary>
+	/// <param name="gameId">The ID of the game to upload files for</param>
+	/// <param name="inDir">The local directory containing save data</param>
+	/// <returns></returns>
+	public abstract Task UploadFiles(string gameId, string inDir);
+
+	/// <summary>
+	/// Returns the total and available space on the remote server.
+	/// </summary>
+	/// <returns>Total and available space on the remote server</returns>
+	public abstract Task<SpaceUssage> GetSpaceUsage();
+}
